ef3c5af36fc5805adf2fe8f0674b9b9919bd9844
surrealbert <albert95web@gmail.com>
2025-05-06 14:19:23 -0700

    v1.0

--- Program.cs
+++ Program.cs
@@ -12,9 +12,9 @@ namespace Green_API_library
             System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
 
             //Data
-            string idInstance = "7103912812";
-            string apiTokenInstance = "e5ddd31c992e46389d8022ac7405057dad8772533be043a99d";
-            string waNumber = "5216641726110";
+            string idInstance = "*****";
+            string apiTokenInstance = "*****";
+            string waNumber = "*****";
 
             //Send a test message
             HttpClient client = new HttpClient();
