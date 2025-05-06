3e307b67d176c22b2f741a4992ab96aa386a81f2
surrealbert <albert95web@gmail.com>
2025-05-06 13:41:52 -0700

    Green-API library

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Green_API_library
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            //Data
+            string idInstance = "7103912812";
+            string apiTokenInstance = "e5ddd31c992e46389d8022ac7405057dad8772533be043a99d";
+            string waNumber = "5216641726110";
+
+            //Send a test message
+            HttpClient client = new HttpClient();
+            WSP_API wsp_request = new WSP_API(idInstance, apiTokenInstance);
+            await wsp_request.Send_WSP_msg(client, waNumber, "Test Message");
+        }
+    }
+}
